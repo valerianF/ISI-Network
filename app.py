d11a1b88389f0e4b14ef03af49a5f459837c6a92
valerianF <valerian.fraisse@gmail.com>
2021-02-25 16:48:40 +0100

    first network attempt

--- app.py
+++ app.py
@@ -5,13 +5,33 @@ import numpy as np
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_cytoscape as cyto
-from dash.dependencies import Input, Output
-import plotly.graph_objects as go
+# from dash.dependencies import Input, Output
+# import plotly.graph_objects as go
 
 from apps.network import netObj
+
+""" App initialization """
+app = dash.Dash(__name__)
 
 """ Importing the datasheet into a pandas dataframe """
 data = pd.read_csv(os.path.join(os.getcwd(), 'data', 'installationsList.csv'))
 
 net = netObj(data)
-net.initiate_network()
+net.initiate_network(['IA_Many', 'IDof_One', 'CO_Exhibition'])
+elements = net.elements
+
+""" Application Layout """
+app.layout = html.Div([
+    cyto.Cytoscape(
+        id='main-network',
+        layout={
+            'name': 'concentric',
+            'padding': '0'
+            },
+        style={'width': '100%', 'height': '800px'},
+        elements = elements
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- apps/network.py
+++ apps/network.py
@@ -1,12 +1,42 @@
 import pandas as pd
+import numpy as np
 
 class netObj:
     def __init__(self, data):
         self.data = data
         self.IDs = []
         self.net = {}
+        self.len = len(self.data)
+        self.elements = []
 
-    def initiate_network(self):
+    def initiate_network(self, keys):
 
-        for i in range(len(self.data)):
-            print(self.data.loc[i, 'Name'])
+        # Nodes elements
+        for i in range(self.len):
+            self.elements.append(dict(
+                data=dict(
+                    id=self.data.loc[i, 'ID'],
+                    label=self.data.loc[i, 'Name']
+                    ),
+                # grabbable=False
+                ))
+
+        # Edge elements
+        array = np.linspace(0, self.len-1, self.len, dtype=int)
+        for i in range(self.len):
+            values_i = [self.data.loc[i, key] for key in keys]
+            for key in keys:
+                values_i.append(self.data.loc[i, key])
+            if all(v == 1 for v in values_i):
+                array = np.delete(array, np.where(array == i))
+                for j in array:
+                    values_j = [self.data.loc[j, key] for key in keys]
+                    if all(v == 1 for v in values_j):
+                        self.elements.append(dict(
+                           data=dict(
+                               source=self.data.loc[i, 'ID'],
+                               target=self.data.loc[j, 'ID'],
+                               label='Node {x} to {y}'.format(x=self.data.loc[i, 'ID'],
+                                                              y=self.data.loc[j, 'ID'])
+                           ) 
+                        ))
