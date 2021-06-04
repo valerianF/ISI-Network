5a72e5d73b16bd5b32db489d02e61e80a66360b5
valerianF <valerian.fraisse@gmail.com>
2021-06-04 17:16:23 +0200

    first try (very buggy...)

--- apps/network.py
+++ apps/network.py
@@ -27,6 +27,8 @@ class netObj:
         self.colors = ['red', 'blue', 'green','magenta', 'cyan', 'black', 'yellow', 'brown']
         self.elements = []
         self.stylesheet = []
+        shared_parents = []
+        is_compound = []
 
         # Initialize parents list
         for col in self.data:
@@ -37,10 +39,45 @@ class netObj:
                         if self.data.loc[i, col] == 1:
                             if col not in self.parents:
                                 self.parents.append(col)
+                                is_compound.append(False)
 
+        # Prior evaluation of shared parents
+        array = np.linspace(0, self.len-1, self.len, dtype=int)
+        for i in range(self.len):
+            values_i = [self.data.loc[i, key] for key in keys]
+            if all(v == 1 for v in values_i):
+                array = np.delete(array, np.where(array == i))
+                node_parent_i = []
+
+                # Evaluate Node's parent(s)
+                for col in self.parents:
+                    if self.data.loc[i, col] == 1:
+                        node_parent_i.append(col)
+
+                # Continue if node doesn't have any parent
+                if node_parent_i == []:
+                    continue
+                
+                # Evaluate remaining installations for siblings
+                for j in array:
+                    values_j = [self.data.loc[j, key] for key in keys]
+                    if all(v == 1 for v in values_j):
+                        node_parent_j = []
+
+                        # Evaluate Node's parent(s)
+                        for col in self.parents:
+                            if self.data.loc[j, col] == 1:
+                                node_parent_j.append(col)
+
+                        if (any([p in node_parent_i for p in node_parent_j])):
+                            n_p = 0
+                            for p in node_parent_i:
+                                if p in node_parent_j:
+                                    n_p += 1
+                                    shared_parents.append(p)
+        
         # Node and Edge elements
         array = np.linspace(0, self.len-1, self.len, dtype=int)
-        shared_parents = []
         for i in range(self.len):
             values_i = [self.data.loc[i, key] for key in keys]
             if all(v == 1 for v in values_i):
@@ -83,46 +120,67 @@ class netObj:
                             for p in node_parent_j:
                                 if p in node_parent_i:
                                     n_p += 1
-                                    shared_parents.append(p)
 
-                            # Add Edges for Nodes with same parent
-                            self.elements.append(dict(
-                            data=dict(
-                                source=self.data.loc[i, 'ID'],
-                                target=self.data.loc[j, 'ID'],
-                                label=node_parent_i
-                            ),
-                            classes= self.colors[self.parents.index(node_parent_i[0])]
-                            ))
-                            if n_p == 2:
+                            if shared_parents.count(node_parent_i[0]) < 5:
+                                # Add Edges for Nodes with same parent
                                 self.elements.append(dict(
                                 data=dict(
                                     source=self.data.loc[i, 'ID'],
                                     target=self.data.loc[j, 'ID'],
-                                    label=node_parent_i
+                                    label=node_parent_i[0]
                                 ),
-                                classes= self.colors[self.parents.index(node_parent_i[1])] +
-                                    " " + 'bezier'
+                                classes= self.colors[self.parents.index(node_parent_i[0])]
                                 ))
-                            if n_p == 3:
-                                self.elements.append(dict(
-                                data=dict(
-                                    source=self.data.loc[i, 'ID'],
-                                    target=self.data.loc[j, 'ID'],
-                                    label=node_parent_i
-                                ),
-                                classes= self.colors[self.parents.index(node_parent_i[2])] +
-                                    " " + 'bezier1'
+                                if n_p == 2:
+                                    if shared_parents.count(node_parent_i[1]) < 5:
+                                        self.elements.append(dict(
+                                        data=dict(
+                                            source=self.data.loc[i, 'ID'],
+                                            target=self.data.loc[j, 'ID'],
+                                            label=node_parent_i[1]
+                                        ),
+                                        classes= self.colors[self.parents.index(node_parent_i[1])] +
+                                            " " + 'bezier'
+                                        ))
+                                    else:
+                                        is_compound[self.parents.index(node_parent_i[1])] = True
+
+                                if n_p == 3:
+                                    if shared_parents.count(node_parent_i[2]) < 5:
+                                        self.elements.append(dict(
+                                        data=dict(
+                                            source=self.data.loc[i, 'ID'],
+                                            target=self.data.loc[j, 'ID'],
+                                            label=node_parent_i[2]
+                                        ),
+                                        classes= self.colors[self.parents.index(node_parent_i[2])] +
+                                            " " + 'bezier1'
+                                        ))
+                                    else:
+                                        is_compound[self.parents.index(node_parent_i[2])] = True
+                            else:
+                                is_compound[self.parents.index(node_parent_i[0])] = True
+
+        for i in range(len(is_compound)):
+            if is_compound[i]:
+                self.elements.append(dict(
+                                    data=dict(
+                                        id=self.parents[i]
+                                    ),
+                                classes= self.colors[i]
                                 ))
-
-        # self.elements.append(dict(
-        #     data=dict(
-        #         id=self.parents[0],
-        #         label=self.parents[0]
-        #     ),
-        # classes= self.colors[0]
-        # ))
-
+                self.stylesheet.append(dict(
+                    selector = '$node > node' + self.colors[i],
+                    style = {
+                        'background-color': self.colors[i],
+                        'background-opacity': '0.2',
+                        'shape': 'roundrectangle',
+                        'text-background-color': self.colors[i],
+                        'text-background-opacity': '0.2',
+                        'border-width': 0.2,
+                    }
+                ))
+                                
         # Generate Stylesheets
         for i in range(len(self.elements)):
             try:
@@ -187,16 +245,4 @@ class netObj:
                 "background-color": "black"
                 }
         ))
-
-        # self.stylesheet.append(dict(
-        #     selector = '$node > node' + self.colors[0],
-        #     style = {
-        #         'background-color': self.colors[0],
-        #         'background-opacity': '0.2',
-        #         'shape': 'roundrectangle',
-        #         'text-background-color': self.colors[0],
-        #         'text-background-opacity': '0.2',
-        #         'border-width': 0.2,
-        #     }
-        # ))
 
